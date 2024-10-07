53767370466ab99ee0bac19e2843766526fb7789
kappi <kappi.patterson@gmail.com>
2024-10-07 19:54:57 +0200

    Using command-based pose resets

--- exts/navigation_template/navigation_template/env_config/__init__.py
+++ exts/navigation_template/navigation_template/env_config/__init__.py
@@ -1,4 +1,2 @@
 from .env_cfg_base import NavigationTemplateEnvCfg_DEV, NavigationTemplateEnvCfg_TRAIN, NavigationTemplateEnvCfg_PLAY
 from . import helper_configurations
-
-__all__ = ["NavigationTemplateEnvCfg_DEV", "NavigationTemplateEnvCfg_TRAIN", "NavigationTemplateEnvCfg_PLAY"]
